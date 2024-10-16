bc76932ee0453d959833b9fcf06373267a864ada
Naufal Rozan <rozan_naufal@yahoo.com>
2024-10-16 14:11:06 +0700

    fix: Mendapatkan prodi_id dari user yang login pada CPMK

--- app/Filament/Resources/CpmkResource/Pages/ListCpmks.php
+++ app/Filament/Resources/CpmkResource/Pages/ListCpmks.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\CpmkResource\Pages;
 use App\Filament\Resources\CpmkResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class ListCpmks extends ListRecords
 {
@@ -17,5 +19,15 @@ class ListCpmks extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        // Mendapatkan prodi_id dari user yang login
+        $user = Auth::user();
+        $prodiIds = $user->prodis->pluck('id')->toArray();
+
+        // Hanya menampilkan data kurikulum sesuai dengan prodi user
+        return parent::getTableQuery()->whereIn('prodi_id', $prodiIds);
     }
 }
