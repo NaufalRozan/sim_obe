492e31ffb7a4b15312a1cfbcbcbfc07c4a6c6b92
Naufal Rozan <rozan_naufal@yahoo.com>
2024-10-24 13:15:05 +0700

    fix: CPMK Matakuliah hanya menampilkan data sesuai prodi

--- app/Filament/Resources/CpmkResource/Pages/ListCpmks.php
+++ app/Filament/Resources/CpmkResource/Pages/ListCpmks.php
@@ -12,7 +12,7 @@ class ListCpmks extends ListRecords
 {
     protected static string $resource = CpmkResource::class;
 
-    public static ?string $title = 'CPMK';
+    public static ?string $title = 'CPMK Matakuliah';
 
     protected function getHeaderActions(): array
     {
@@ -21,15 +21,18 @@ class ListCpmks extends ListRecords
         ];
     }
 
-    // protected function getTableQuery(): Builder
-    // {
-    //     // Mendapatkan prodi_id dari user yang login
-    //     $user = Auth::user();
-    //     $kurikulumIds = $user->prodis->flatMap(function ($prodi) {
-    //         return $prodi->kurikulums->pluck('id');
-    //     })->toArray();
+    protected function getTableQuery(): Builder
+    {
+        // Mendapatkan prodi_id dari user yang login
+        $user = Auth::user();
+        $kurikulumIds = $user->prodis->flatMap(function ($prodi) {
+            return $prodi->kurikulums->pluck('id');
+        })->toArray();
 
-    //     // Hanya menampilkan data cpl sesuai dengan kurikulum user
-    //     return parent::getTableQuery()->whereIn('kurikulum_id', $kurikulumIds);
-    // }
+        // Query dengan join ke tabel mk untuk membatasi berdasarkan kurikulum
+        return parent::getTableQuery()
+            ->whereHas('mk', function (Builder $query) use ($kurikulumIds) {
+                $query->whereIn('kurikulum_id', $kurikulumIds);
+            });
+    }
 }
