20c6006b5446867f0609bebd28f852f09621cbf0
Naufal Rozan <rozan_naufal@yahoo.com>
2024-11-14 22:00:00 +0700

    fix: konfigurasi tailwind css

--- resources/views/filament/custom/upload-file.blade.php
+++ resources/views/filament/custom/upload-file.blade.php
@@ -1,49 +1,36 @@
-<style>
-    .bg-success {
-        background-color: #4caf50;
-    }
+<x-layouts.app>
+    <div>
+        <x-filament::breadcrumbs :breadcrumbs="[
+            '/pengajar/cpmk-pengajars' => 'CPMK Matakuliah',
+            '' => 'List',
+        ]" />
 
-    .bg-success:hover {
-        background-color: #43a047;
-    }
+        <div class="flex justify-between items-center mt-4">
+            <div class="text-3xl font-bold">Nilai CPMK</div>
+            <div>
+                {{ $downloadExcelAction }}
+            </div>
+        </div>
 
-    .input-margin {
-        margin-right: 8px;
-        /* Menambahkan margin kanan pada input */
-    }
-</style>
-
-<div>
-    <x-filament::breadcrumbs :breadcrumbs="[
-        '/pengajar/cpmk-pengajars' => 'CPMK Matakuliah',
-        '' => 'List',
-    ]" />
-    <div class="flex justify-between mt-4">
-        <div class="font-bold text-3xl">Nilai CPMK</div>
-        <div>
-            {{ $downloadExcelAction }}
+        <div class="mt-6">
+            <form wire:submit.prevent="save" class="w-full max-w-sm flex flex-col space-y-4">
+                <div>
+                    <label for="fileInput" class="block text-sm font-bold text-gray-700 mb-2">
+                        Pilih Berkas
+                    </label>
+                    <div class="flex items-center space-x-4">
+                        <input id="fileInput" type="file" wire:model="file"
+                            class="w-full px-3 py-2 border rounded shadow appearance-none text-gray-700 focus:outline-none focus:shadow-outline">
+                        <button type="submit"
+                            class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700 focus:outline-none focus:shadow-outline">
+                            Upload
+                        </button>
+                    </div>
+                    @error('file')
+                        <span class="text-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+            </form>
         </div>
     </div>
-    <div class="mt-4">
-        <form wire:submit.prevent="save" class="w-full max-w-sm flex flex-col">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="fileInput">
-                    Pilih Berkas
-                </label>
-                <div class="flex items-center">
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline input-margin"
-                        id="fileInput" type="file" wire:model="file">
-                    <button
-                        class="bg-success hover:bg-success-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit">
-                        Upload
-                    </button>
-                </div>
-                @error('file')
-                    <span class="text-red-500 text-sm">{{ $message }}</span>
-                @enderror
-            </div>
-        </form>
-    </div>
-</div>
+</x-layouts.app>
